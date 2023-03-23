67c46cea9283242d8b116e081238dd0ac583553b
Jasvinder Singh <jasvinder.rajput@cylsys.com>
2023-03-23 12:25:36 +0530

    settings issue fixed

--- src/app/features/settings/components/settings-main/settings-main.component.ts
+++ src/app/features/settings/components/settings-main/settings-main.component.ts
@@ -9,6 +9,8 @@ import { SharedService } from '@shared/services/shared.service';
 export class SettingsMainComponent implements OnInit {
 
   isLeftMenuOpen: boolean;
+  mainClass:String = 'col-md-10';
+  sideBarClass:String = 'mobile-menu';
 
   constructor(
     private _sharedService:SharedService) { }
@@ -16,6 +18,16 @@ export class SettingsMainComponent implements OnInit {
   ngOnInit(): void {
     this._sharedService.leftMenuStatus.subscribe((res: any) => {
       this.isLeftMenuOpen = res.leftMenuOpen;
+      console.log(this.sideBarClass);
+      if(this.isLeftMenuOpen){
+        console.log('1');
+        this.sideBarClass = 'mobile-menu';
+        this.mainClass = 'col-md-10';
+      } else {
+        console.log('2');
+        this.sideBarClass = '';
+        this.mainClass = 'col-md-12';
+      }
     });
   }
 

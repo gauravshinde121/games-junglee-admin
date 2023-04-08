3c2296f2e3950c181a99c3892ffeed24d09b0ee3
brijesh <brijeshdubeyid@gmail.com>
2023-04-08 14:45:48 +0530

    resolve

--- src/app/features/book-management/components/net-exposure/net-exposure-view-total/net-exposure-view-total.component.ts
+++ src/app/features/book-management/components/net-exposure/net-exposure-view-total/net-exposure-view-total.component.ts
@@ -150,7 +150,6 @@ export class NetExposureViewTotalComponent implements OnInit {
       // console.log('data', JSON.parse(data));
         let webSocketData = parseData['data'];
         this.adminBooksList.map((singleBook)=>{
-          console.log(webSocketData,singleBook['marketId']);
           let singleWebSocketMarketData = _.find(webSocketData, ['bmi', +singleBook['marketId']]);
           if(singleWebSocketMarketData != undefined){
               return singleBook['adminBook'].map((runnerRes) => {

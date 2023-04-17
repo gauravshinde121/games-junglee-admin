f0f692e0359ad272cb444fdf7ca17c4389ba2fc5
brijesh <brijeshdubeyid@gmail.com>
2023-04-17 17:26:49 +0530

    added tv and websocket

--- src/app/config/constant.ts
+++ src/app/config/constant.ts
@@ -98,4 +98,6 @@ export class Constants {
 
     //pubsub
     public readonly API_URL_GET_USER_ADMIN_PUB_SUB:string = 'api/getUserAdminPubSub';
+    public readonly API_URL_GET_WEBSOCKET_URL: string = 'api/pubsuburl';
+    public readonly API_URL_LIVE_STREAM_MATCH:string = 'api/getStreamingUrl';
 }

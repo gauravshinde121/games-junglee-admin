f19a48762a31d3a8b6428c2c9ac16167aa160f94
gauravshinde121 <gaurav.shinde@cylsys.com>
2024-02-14 15:25:33 +0530

    added custom socket listeneing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,5 +7,6 @@ export const environment = {
   appEnv: 'production',
   OcpApimSubscriptionKey:'131ad35b6d6e48c597bb466343d18b2d',
   JungleeToken:'RANDOM_GUID',
-  clientId:1
+  clientId:1,
+  oddsSocketUrl:'wss://odds.cylsys.com'
 };

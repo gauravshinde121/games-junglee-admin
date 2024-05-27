a1c59857f7f2691be42427637e64041572c4e33a
Rupesh Narkar <rupeshnarkar18@gmail.com>
2024-05-27 18:27:06 +0530

    delete components

--- src/app/features/all-logs/all-logs-routing.module.ts
+++ src/app/features/all-logs/all-logs-routing.module.ts
@@ -6,9 +6,6 @@ import { ReportsComponent } from './components/reports/reports.component';
 import { ClTransferComponent } from './components/cl-transfer/cl-transfer.component';
 import { BetfairRateLogComponent } from './components/betfair-rate-log/betfair-rate-log.component';
 import { InvalidBetComponent } from './components/invalid-bet/invalid-bet.component';
-import { LoginFromBlockedIpComponent } from './components/login-from-blocked-ip/login-from-blocked-ip.component';
-import { BlockedIpComponent } from './components/blocked-ip/blocked-ip.component';
-import { MultipleIpComponent } from './components/multiple-ip/multiple-ip.component';
 
 const routes: Routes = [
   {path:'all-logs',component:AllLogsMainComponent,
@@ -18,9 +15,6 @@ children:[
   {path:'cl-transfer',component:ClTransferComponent},
   {path:'betfair-rate-log',component:BetfairRateLogComponent},
   {path:'invalid-bet',component:InvalidBetComponent},
-  {path:'multiple-ip',component:MultipleIpComponent},
-  {path:'blocked-ip',component:BlockedIpComponent},
-  {path:'login-from-blocked-ip',component:LoginFromBlockedIpComponent},
 ]}
 ];
 
